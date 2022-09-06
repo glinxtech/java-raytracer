f3479b5db38842f8b03bb93512d9c9de0ee05d6a
glinxtech <gooplsarrero@gmail.com>
2022-09-06 18:10:29 +0200

    Fixed Docs to comply with Oracle convention

--- src/utils/Logger.java
+++ src/utils/Logger.java
@@ -7,7 +7,12 @@ public class Logger
 {
     private static int logLevel = 0;
 
-    // If the set logLevel is equal or higher to the level used when logging, it will print
+    /**
+     * Prints log message if the set logLevel is higher than that of the message.
+     *
+     * @param msg the log message
+     * @param level the priority of log for given message
+     */
     public static void log(String msg, int level)
     {
         if (level <= Logger.logLevel)
@@ -16,6 +21,11 @@ public class Logger
         }
     }
 
+    /**
+     * Sets our current log level.
+     *
+     * @param level the new level value
+     */
     public static void setLogLevel(int level)
     {
         Logger.logLevel = level;
