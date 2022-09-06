21ca0cefa8a644b0584bb6d22e7cbb06b0ea2291
glinxtech <gooplsarrero@gmail.com>
2022-09-06 23:19:11 +0200

    Added materials

---
+++ src/raytracer/Colour.java
@@ -0,0 +1,15 @@
+package raytracer;
+
+public class Colour
+{
+    private double red;
+    private double green;
+    private double blue;
+
+    public Colour(double red, double green, double blue)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
