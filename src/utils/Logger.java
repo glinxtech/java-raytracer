d788999cc4a57c665b576d3e4802e4f2f81ce2ea
glinxtech <gooplsarrero@gmail.com>
2022-09-06 18:02:25 +0200

    Logger comments

--- src/utils/Logger.java
+++ src/utils/Logger.java
@@ -1,10 +1,13 @@
 package utils;
 
-
+/**
+ * Wrapper for logging
+ */
 public class Logger
 {
     private static int logLevel = 0;
 
+    // If the set logLevel is equal or higher to the level used when logging, it will print
     public static void log(String msg, int level)
     {
         if (level <= Logger.logLevel)
