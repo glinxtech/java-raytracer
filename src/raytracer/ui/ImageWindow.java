57f2382f691a49cb6988e726e5d61e1dc3087893
Klaas <thefrisiangamer@gmail.com>
2022-10-12 14:42:30 +0200

    Added menubar and beginnings of File and Settings options

--- src/raytracer/ui/ImageWindow.java
+++ src/raytracer/ui/ImageWindow.java
@@ -2,10 +2,14 @@ package raytracer.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-public class ImageWindow
+public class ImageWindow extends JFrame implements ActionListener
 {
+    private JMenu menu;
+    private JMenuItem file, setting, close;
     private JFrame f;
 
     public ImageWindow(BufferedImage img)
@@ -24,10 +28,48 @@ public class ImageWindow
         // Add image to our window
         f.add(imagePanel);
 
+        // Set up menubar
+        JMenuBar mb = new JMenuBar();
+        menu = new JMenu("File");
+        file = new JMenuItem("Open File");
+        file.addActionListener(this);
+        setting = new JMenuItem("Settings");
+        setting.addActionListener(this);
+        close = new JMenuItem("Exit");
+        close.addActionListener(this);
+        menu.add(file);
+        menu.add(setting);
+        menu.add(close);
+        mb.add(menu);
+        f.setJMenuBar(mb);
+
+
         // Reveal window
         f.setVisible(true);
 
         // This makes our application able to be shutdown by closing this window
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    // Actions for the menu bar
+    public void actionPerformed(ActionEvent e)
+    {
+        // If-statement for each action, as switches don't like ActionEvent
+
+        // Opens file chooser
+        if (e.getSource() == file)
+        {
+            JFileChooser fc = new JFileChooser();
+            int i = fc.showOpenDialog(this);
+            // TODO: Finish when able to load objects
+        }
+
+        // Opens settings menu
+        if (e.getSource() == setting)
+            new SettingsWindow();
+
+        // Exits out of the program
+        if (e.getSource() == close)
+            System.exit(0);
     }
 }
---
+++ src/raytracer/ui/SettingsWindow.java
@@ -0,0 +1,15 @@
+// TODO: Finish settings window
+
+package raytracer.ui;
+
+import javax.swing.*;
+
+public class SettingsWindow
+{
+    private JFrame f;
+
+    public SettingsWindow()
+    {
+
+    }
+}
